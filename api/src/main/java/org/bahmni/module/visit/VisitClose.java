47c9dc1cdbd7edb15fcacd0708f370ae39230742
Siva Rachakonda <shiva.rachakonda139@gmail.com>
2019-07-15 19:20:34 +0530

    Siva R| MOBN-557 | Read concept names from global properties

--- api/src/main/java/org/bahmni/module/visit/VisitClose.java
+++ api/src/main/java/org/bahmni/module/visit/VisitClose.java
@@ -1,27 +1,34 @@
 package org.bahmni.module.visit;
 
 import org.apache.commons.lang3.StringUtils;
+import org.openmrs.Concept;
 import org.openmrs.VisitType;
+import org.openmrs.api.ConceptService;
 import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class VisitClose {
 
-    private static final String VISIT_TYPE_GLOBAL_PROPERTIEY = "visits.closeOnAnOutcome.visitType(s)";
+    private static final String VISIT_TYPES_GLOBAL_PROPERTY = "visits.closeOnAnOutcome.visitType(s)";
+    private static final String CONCEPTS_GLOBAL_PROPERTY = "visits.closeOnAnOutcome.conceptName(s)";
     private GlobalPropertyReader globalPropertyReader;
     private VisitService visitService;
+    private ConceptService conceptService;
 
     VisitClose() {
         this.globalPropertyReader = new GlobalPropertyReader();
         this.visitService = Context.getVisitService();
+        this.conceptService = Context.getConceptService();
     }
 
     List<VisitType> getVisitTypes() {
         ArrayList<VisitType> visitTypes = new ArrayList<>();
-        String commaSeparatedVisitTypes = globalPropertyReader.getValueOfProperty(VISIT_TYPE_GLOBAL_PROPERTIEY);
+        String commaSeparatedVisitTypes = globalPropertyReader.getValueOfProperty(VISIT_TYPES_GLOBAL_PROPERTY);
         if (StringUtils.isBlank(commaSeparatedVisitTypes))
             return visitTypes;
         String[] visitTypeNames = commaSeparatedVisitTypes.split("\\s*,\\s*");
@@ -31,5 +38,15 @@ class VisitClose {
         }
         return visitTypes;
     }
+
+    List<Concept> getConcepts() {
+        ArrayList<Concept> concepts = new ArrayList<>();
+        String pipeSeparatedConceptNames = globalPropertyReader.getValueOfProperty(CONCEPTS_GLOBAL_PROPERTY);
+        if (StringUtils.isBlank(pipeSeparatedConceptNames))
+            return concepts;
+        String[] fullyQualifiedConceptNames = pipeSeparatedConceptNames.split("\\s*\\|\\s*");
+        return Arrays.stream(fullyQualifiedConceptNames).map(conceptService::getConcept).collect(Collectors.toList());
+    }
+
 
 }
--- api/src/test/org/bahmni/module/visit/VisitCloseTest.java
+++ api/src/test/org/bahmni/module/visit/VisitCloseTest.java
@@ -4,7 +4,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.openmrs.Concept;
 import org.openmrs.VisitType;
+import org.openmrs.api.ConceptService;
 import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
 import org.powermock.api.mockito.PowerMockito;
@@ -30,18 +32,21 @@ public class VisitCloseTest {
     private GlobalPropertyReader globalPropertyReader;
     @Mock
     private VisitService visitService;
+    @Mock
+    private ConceptService conceptService;
 
     @Before
     public void setUp() throws Exception {
         PowerMockito.mockStatic(Context.class);
         when(Context.getVisitService()).thenReturn(visitService);
+        when(Context.getConceptService()).thenReturn(conceptService);
 
         visitClose = new VisitClose();
         setValuesForMemberFields(visitClose, "globalPropertyReader", globalPropertyReader);
     }
 
     @Test
-    public void shouldReturnVisitTypeObjectForGivenVisitTypeName() {
+    public void shouldReturnVisitTypeObjectForGivenVisitTypeNameInGlobalProperty() {
         String visitTypeNames = "MLO, OPD";
         VisitType mloVisitType = mock(VisitType.class);
         VisitType opdVisitType = mock(VisitType.class);
@@ -65,5 +70,26 @@ public class VisitCloseTest {
         List<VisitType> visitTypes = visitClose.getVisitTypes();
 
         assertEquals(0, visitTypes.size());
+    }
+
+    @Test
+    public void shouldConceptsForGivenConceptNamesInGlobalPropery() {
+        String conceptNames = "FUP, Outcomes for follow-up surgical validation | FV, Outcomes FV";
+        Concept concept1 = mock(Concept.class);
+        Concept concept2 = mock(Concept.class);
+        when(globalPropertyReader.getValueOfProperty("visits.closeOnAnOutcome.conceptName(s)"))
+                .thenReturn(conceptNames);
+
+        when(conceptService.getConcept("FUP, Outcomes for follow-up surgical validation"))
+                .thenReturn(concept1);
+        when(conceptService.getConcept("FV, Outcomes FV"))
+                .thenReturn(concept2);
+
+
+        List<Concept> concepts = visitClose.getConcepts();
+
+        assertEquals(2, concepts.size());
+        assertTrue(concepts.containsAll(Arrays.asList(concept1, concept2)));
+
     }
 }
