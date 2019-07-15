517d205bc3060bfb637d56826134176b6ae4d69c
Siva Rachakonda <shiva.rachakonda139@gmail.com>
2019-07-16 00:20:55 +0530

    Siva R| MOBN-557 | Add task to close visits based on concept outcome

---
+++ api/src/main/java/org/bahmni/module/visit/CloseVisitOnAnOutcomeTask.java
@@ -0,0 +1,39 @@
+package org.bahmni.module.visit;
+
+import org.bahmni.module.bahmnicore.service.BahmniObsService;
+import org.openmrs.Concept;
+import org.openmrs.Visit;
+import org.openmrs.api.VisitService;
+import org.openmrs.api.context.Context;
+import org.openmrs.scheduler.tasks.AbstractTask;
+
+import java.util.Date;
+import java.util.List;
+
+public class CloseVisitOnAnOutcomeTask extends AbstractTask {
+
+    private VisitService visitService;
+    private VisitCloseData visitCloseData;
+
+    CloseVisitOnAnOutcomeTask() {
+        super();
+        visitService = Context.getVisitService();
+        visitCloseData = new VisitCloseData();
+    }
+
+    @Override
+    public void execute() {
+        List<Visit> visits = visitService.getVisits(visitCloseData.getVisitTypes(), null, null,
+                null, null, null, null, null, null, false, false);
+
+        visits.forEach(openVisit -> {
+            if (outcomeAvailable(openVisit, visitCloseData.getConcepts()))
+                visitService.endVisit(openVisit, new Date());
+        });
+    }
+
+    private boolean outcomeAvailable(Visit openVisit, List<Concept> concepts) {
+        BahmniObsService bahmniObsService = Context.getService(BahmniObsService.class);
+        return !bahmniObsService.getLatestObsByVisit(openVisit, concepts, null, true).isEmpty();
+    }
+}
---
+++ api/src/test/org/bahmni/module/visit/CloseVisitOnAnOutcomeTaskTest.java
@@ -0,0 +1,100 @@
+package org.bahmni.module.visit;
+
+import org.bahmni.module.bahmnicore.service.BahmniObsService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.openmrs.Concept;
+import org.openmrs.Visit;
+import org.openmrs.VisitType;
+import org.openmrs.api.VisitService;
+import org.openmrs.api.context.Context;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static org.bahmni.module.visit.TestHelper.setValuesForMemberFields;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyCollectionOf;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@PrepareForTest(Context.class)
+@RunWith(PowerMockRunner.class)
+public class CloseVisitOnAnOutcomeTaskTest {
+
+    @Mock
+    private VisitService visitService;
+
+    @Mock
+    private VisitCloseData visitCloseData;
+
+    private CloseVisitOnAnOutcomeTask closeVisitOnAnOutcomeTask;
+
+    @Before
+    public void setUp() throws Exception {
+        PowerMockito.mockStatic(Context.class);
+        when(Context.getVisitService()).thenReturn(visitService);
+
+        closeVisitOnAnOutcomeTask = new CloseVisitOnAnOutcomeTask();
+
+        setValuesForMemberFields(closeVisitOnAnOutcomeTask, "visitCloseData", visitCloseData);
+
+    }
+
+    @Test
+    public void shouldVerifyEndVisitIsCalledWhenAnyObsAvailableForGivenConcepts() {
+        VisitType visitType = mock(VisitType.class);
+        List<VisitType> visitTypes = singletonList(visitType);
+        Visit visit = mock(Visit.class);
+        Concept concept = mock(Concept.class);
+        List<Concept> concepts = singletonList(concept);
+        BahmniObsService obsService = mock(BahmniObsService.class);
+
+
+        when(visitCloseData.getVisitTypes()).thenReturn(visitTypes);
+        when(visitService.getVisits(anyCollectionOf(VisitType.class), any(), any(),
+                any(), any(), any(), any(), any(), any(), any(Boolean.class), any(Boolean.class)))
+                .thenReturn(singletonList(visit));
+        when(visitCloseData.getConcepts()).thenReturn(concepts);
+        when(Context.getService(BahmniObsService.class)).thenReturn(obsService);
+        when(obsService.getLatestObsByVisit(any(Visit.class), anyCollectionOf(Concept.class), any(), any()))
+                .thenReturn(singletonList(null));
+
+        closeVisitOnAnOutcomeTask.execute();
+
+        verify(visitService).endVisit(any(Visit.class), any(Date.class));
+    }
+
+    @Test
+    public void shouldVerifyEndVisitIsNotCalledWhenAnyObsAvailableForGivenConcepts() {
+        VisitType visitType = mock(VisitType.class);
+        List<VisitType> visitTypes = singletonList(visitType);
+        Visit visit = mock(Visit.class);
+        Concept concept = mock(Concept.class);
+        List<Concept> concepts = singletonList(concept);
+        BahmniObsService obsService = mock(BahmniObsService.class);
+
+
+        when(visitCloseData.getVisitTypes()).thenReturn(visitTypes);
+        when(visitService.getVisits(anyCollectionOf(VisitType.class), any(), any(),
+                any(), any(), any(), any(), any(), any(), any(Boolean.class), any(Boolean.class)))
+                .thenReturn(singletonList(visit));
+        when(visitCloseData.getConcepts()).thenReturn(concepts);
+        when(Context.getService(BahmniObsService.class)).thenReturn(obsService);
+        when(obsService.getLatestObsByVisit(any(Visit.class), anyCollectionOf(Concept.class), any(), any()))
+                .thenReturn(Collections.emptyList());
+
+        closeVisitOnAnOutcomeTask.execute();
+
+        verify(visitService, times(0)).endVisit(any(Visit.class), any(Date.class));
+    }
+}
