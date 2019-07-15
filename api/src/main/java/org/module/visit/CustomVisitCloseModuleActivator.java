07491534f1a04d3c14cc839df97c35d87acef5ea
Siva Rachakonda <shiva.rachakonda139@gmail.com>
2019-07-15 13:34:59 +0530

    Siva R| MOBN-557 | Add module activator

---
+++ api/src/main/java/org/module/visit/CustomVisitCloseModuleActivator.java
@@ -0,0 +1,52 @@
+package org.module.visit;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.ModuleActivator;
+
+public class CustomVisitCloseModuleActivator implements ModuleActivator {
+
+    private Log log = LogFactory.getLog(getClass());
+
+    /**
+     * @see ModuleActivator#willRefreshContext()
+     */
+    public void willRefreshContext() {
+        log.info("Refreshing visit-close-module");
+    }
+
+    /**
+     * @see ModuleActivator#contextRefreshed()
+     */
+    public void contextRefreshed() {
+        log.info("visit-close-module refreshed");
+    }
+
+    /**
+     * @see ModuleActivator#willStart()
+     */
+    public void willStart() {
+        log.info("Starting visit-close-module ");
+    }
+
+    /**
+     * @see ModuleActivator#started()
+     */
+    public void started() {
+        log.info("visit-close-module started");
+    }
+
+    /**
+     * @see ModuleActivator#willStop()
+     */
+    public void willStop() {
+        log.info("Stopping visit-close-module ");
+    }
+
+    /**
+     * @see ModuleActivator#stopped()
+     */
+    public void stopped() {
+        log.info("visit-close-module stopped");
+    }
+}
